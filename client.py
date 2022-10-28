ac666848865217fd4eb3b80eae7ab42a09ccfa76
Nguyễn Đức <ducna1462@gmail.com>
2022-10-28 16:55:25 +0700

    check 2 number

--- client.py
+++ client.py
@@ -9,24 +9,39 @@ qstData = {}
 text_cache = ''
 
 questionNumber = 1
+secondNumber = -1
+secondNumberActive = False
 active = 0
-
-
-print(qstData.keys())
-
 
 for page in fileReader.pages:
     for text in page.extract_text():
 
-        text_cache += f'{text}'
+        text_cache += f'{text}'        
         
         if text == f'{questionNumber}':
             active = 1
-        if active == 1 and text == '.':
-            active = 0
-            qstData[f'{questionNumber}']  = f'{text_cache}'
-            text_cache = ''
-            break
-    break
-print(qstData['1'])
-    
+        if active == 1:
+            if text == f'{secondNumber}' and secondNumberActive:
+                pass
+            elif text == '.':
+                active = 0
+                key = questionNumber if not secondNumberActive else questionNumber*10 + secondNumber
+                qstData[f'{key}']  = f'Q{questionNumber - 1}{text_cache[:-2]}'
+                text_cache = ''
+                if questionNumber == 9:
+                    questionNumber = 1
+                    secondNumberActive = True
+
+                elif not secondNumberActive:
+                    questionNumber += 1
+                if secondNumberActive:
+                    if secondNumber == 9:
+                        secondNumber = 0
+                        questionNumber += 1
+                    else:
+                        secondNumber += 1
+key = questionNumber if not secondNumberActive else questionNumber*10 + secondNumber
+qstData[f'{key}']  = f'Q{questionNumber - 1}{text_cache[:-2]}'                    
+
+for i in qstData.values():
+    print(i)   
